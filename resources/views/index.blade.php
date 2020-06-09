e46186c072bb344932de259506b9ecbb50f6787d
Nikotine <nikotine991@gmail.com>
2020-06-09 15:17:15 -0400

    se confuhuran botones de navbar

---
+++ resources/views/index.blade.php
@@ -0,0 +1,12 @@
+@extends('partials.maincontainer')
+@section('content')
+
+<h1>Inicio</h1>
+
+
+
+
+
+
+
+@endsection
--- resources/views/orders/add.blade.php
+++ resources/views/orders/add.blade.php
@@ -1,6 +1,4 @@
 @extends('partials.maincontainer')
-
-
 @section('content')
 
 
@@ -16,22 +14,22 @@
 <br>
 <label>
     Direccion <br>
-    <input type="text" name="address" value="{{ old('address', '') }}">
+    <input type="text" class="form-control" name="address" value="{{ old('address', '') }}">
 </label>
 <br>
 <label>
     Fecha <br>
-    <input type="date" name="deliver_date" value="{{ old('delver_date', '') }}">
+    <input type="date" class="form-control" name="deliver_date" value="{{ old('delver_date', '') }}">
 </label>
 <br>
 <label>
     Hora <br>
-    <input type="time" name="deliver_hour" value="{{ old('delver_hour', '') }}">
+    <input type="time" class="form-control" name="deliver_hour" value="{{ old('delver_hour', '') }}">
 </label>
 <br>
 <label>
     Valor declarado <br>
-    <input type="decimal" name="cost" value="{{ old('cost', '') }}">
+    <input type="integer" class="form-control" name="cost" value="{{ old('cost', '') }}">
 </label>
 <br>
 <button class="btn btn-primary">Guardar</button>
