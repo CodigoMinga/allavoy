8ea5c5cb734ebf24c8f881448baf0bed09e0b0d0
Osvaldo Alvarado <osvaldo.alvarado.dev@gmail.com>
2020-07-07 11:04:53 -0400

    Añade roles al sistema

--- database/seeds/Userseeder.php
+++ database/seeds/Userseeder.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Seeder;
 use App\User;
+use App\Role;
 
 class Userseeder extends Seeder
 {
@@ -12,40 +13,51 @@ class Userseeder extends Seeder
      */
     public function run()
     {
-        $user = new User();
+        $administrador  = Role::where('name', 'administrador')->first();
+        $repartidor      = Role::where('name', 'repartidor')->first();
+
+
+
+            $user = new User();
             $user->name = 'Nikotine';
             $user->email = 'Nikotine991@gmail.com';
             $user->password = bcrypt('password');
             $user->save();
+             $user->roles()->attach($administrador);
 
             $user = new User();
             $user->name = 'luchito';
             $user->email = 'luchito@gmail.com';
             $user->password = bcrypt('password');
             $user->save();
+             $user->roles()->attach($repartidor);
 
             $user = new User();
             $user->name = 'chelo';
             $user->email = 'chelo@gmail.com';
             $user->password = bcrypt('password');
             $user->save();
+            $user->roles()->attach($repartidor);
 
             $user = new User();
             $user->name = 'chami';
             $user->email = 'chami@gmail.com';
             $user->password = bcrypt('password');
             $user->save();
+            $user->roles()->attach($repartidor);
 
             $user = new User();
             $user->name = 'pancho';
             $user->email = 'pancho@gmail.com';
             $user->password = bcrypt('password');
             $user->save();
+             $user->roles()->attach($repartidor);
 
             $user = new User();
             $user->name = 'yari';
             $user->email = 'yari@gmail.com';
             $user->password = bcrypt('password');
             $user->save();
+            $user->roles()->attach($repartidor);
     }
 }
